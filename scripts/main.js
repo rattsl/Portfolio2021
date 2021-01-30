8d3557c54a08d5c63710c0fe46ef9a6c8f3e1c4f
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2021-01-31 04:20:23 +0900

    feat✨: implement Skills section animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,28 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-});
+document.addEventListener('DOMContentLoaded', function(){
+  const skills = new SkillsSlider();
+})
+
+class SkillsSlider{
+  constructor(){
+    this.swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      loop: true,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 2,
+      speed: 1000,
+      breakpoints: {
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 6,
+        }
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      }
+    });
+  }
+}
