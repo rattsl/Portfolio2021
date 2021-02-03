f3de66cbe2c8e68f601e33d70fe270458cbcab6f
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2021-02-04 03:29:54 +0900

    feat✨: implement iTyped.js

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,34 @@ class SkillsSlider{
     });
   }
 }
+
+const hero = document.querySelector('.hero');
+const header = document.querySelector('.header');
+const btn = document.querySelector('.mobile-menu__btn')
+const cb = function(entries, observer){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      header.classList.add('inview');
+      btn.classList.add('inview');
+    } else {
+      header.classList.remove('inview');
+      btn.classList.remove('inview');
+    }
+  });
+}
+
+const io = new IntersectionObserver(cb);
+io.observe(hero);
+
+const el = document.querySelector("#ityped")
+
+ityped.init(el, {
+  strings: ["Hello, World! ","Welcome to my Portfolio Site!","I'm rattsl. Developer, creator.","Enjoy This Website!","RATTSL.COM"],
+  startDelay: 200,
+  typeSpeed: 90,
+  loop: false,
+  backSpeed:  60,
+  backDelay:  300,
+  showCursor: true,
+  cursorChar: "|",
+})
